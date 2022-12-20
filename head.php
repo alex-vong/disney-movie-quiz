13b879efa329c2453301db4e668f1ce6f68fa182
Alex Vong <avong003@gmail.com>
2022-12-20 10:55:13 -0800

    changed head content

---
+++ head.php
@@ -0,0 +1,21 @@
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Basic CSS Quiz</title>
+		<meta
+			name="og:description"
+			content="Test Your Knowledge on CCSS!"
+		/>
+	
+		<meta property="og:title" content="CSS Basics Quiz" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="https://alexvong.dev/images/bros.png"  />
+		<meta property="og:image:type" content="https://alexvong.dev/images/bros.png" />
+		<link rel="shortcut icon" type="image/png" href=[[]] />
+
+		<link rel='stylesheet' href='css/style.css'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
